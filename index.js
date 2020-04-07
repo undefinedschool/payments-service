6ed6d4c495a42d47949716827a793885a4ff29a4
nhsz <nh.quiroz@gmail.com>
2020-04-07 05:14:36 -0300

    feat: add entry point

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
 const router = require('./routes');
 
 const { PORT = 8080 } = process.env;
 const app = express();
 
-app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(express.json());
 app.use(router);
 
 const server = app.listen(PORT);
