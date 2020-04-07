df3faa4ae52cb8e73d88c25c7cbc789ca707b5e7
nhsz <nh.quiroz@gmail.com>
2020-04-07 20:14:28 -0300

    chore: improve routes error handling

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,20 @@ const router = express.Router();
 router.all('*', cors());
 
 router.route('/mercadopago').post(async (req, res) => {
-  const { paymentType } = req.query;
-  const init_point = await generatePayment(req.body, paymentType);
+  try {
+    const { paymentType } = req.query;
+    const init_point = await generatePayment(req.body, paymentType);
 
-  res.send(init_point);
+    res.send(init_point);
+  } catch (e) {
+    console.error(e);
+
+    if (!['card', 'mp'].includes(paymentType)) {
+      res.status(400).send('Invalid payment type');
+    } else {
+      res.status(400).send('Invalid JSON payload');
+    }
+  }
 });
 
 module.exports = router;
