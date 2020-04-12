7474c4a9f21697278dc7313488b31145554c9082
nhsz <nh.quiroz@gmail.com>
2020-04-12 03:15:53 -0300

    feat: add HEAD request support

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,24 @@ router.get('/', (req, res) => res.sendFile('./public/index.html'));
 
 router.get('/health', (req, res) => res.sendStatus(200));
 
-router.post('/mercadopago', async (req, res) => {
-  try {
-    const { paymentType } = req.query;
-    const init_point = await generatePayment(req.body, paymentType);
+router
+  .route('/mercadopago')
+  .head((req, res) => res.sendStatus(200))
+  .post('/mercadopago', async (req, res) => {
+    try {
+      const { paymentType } = req.query;
+      const init_point = await generatePayment(req.body, paymentType);
 
-    res.json({ CHECKOUT_URL: init_point });
-  } catch (e) {
-    console.error(e);
+      res.json({ CHECKOUT_URL: init_point });
+    } catch (e) {
+      console.error(e);
 
-    if (!['card', 'mp'].includes(paymentType)) {
-      res.status(400).send('Invalid payment type');
-    } else {
-      res.status(400).send('Invalid JSON payload');
+      if (!['card', 'mp'].includes(paymentType)) {
+        res.status(400).send('Invalid payment type');
+      } else {
+        res.status(400).send('Invalid JSON payload');
+      }
     }
-  }
-});
+  });
 
 module.exports = router;
